3783ae8ea4e14553a176107168da79b84e03b6f3
rayan439 <muhammadryan2255@gmail.com>
2025-07-26 23:30:42 +0500

    Add files via upload

---
+++ muliplication.table .generator.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main() {
+    int num, count = 1, ans ;
+    printf ("Enter any number:") ;
+  scanf (" %d",&num):
+  while (count<-10)
+{
+   ans - num*
+  printf ( "%d x %D - %d/n",num, count,ams) ;
+  continue;
+
+  }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
